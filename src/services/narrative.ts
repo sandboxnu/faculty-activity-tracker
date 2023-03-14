ef4126ac0f45e0278c52e7667e763acd04133513
Christina Long <long.c@husky.neu.edu>
2023-03-14 19:28:24 -0400

    clarified data types

--- src/services/narrative.ts
+++ src/services/narrative.ts
@@ -2,7 +2,7 @@ import { PrismaClient, Narrative } from '.prisma/client';
 import {
   CreateNarrativeDto,
   UpdateNarrativeDto,
-  NarrativeDto,
+  DeleteNarrativeDto,
 } from '@/models/narrative.model';
 
 const prisma = new PrismaClient();
@@ -19,7 +19,7 @@ export const createNarrative = async (
 };
 
 export const updateNarrative = async (
-  narrative: Partial<NarrativeDto>,
+  narrative: UpdateNarrativeDto,
 ): Promise<Narrative> => {
   const updatedNarrative = await prisma.narrative.update({
     where: { id: narrative.id },
@@ -29,7 +29,7 @@ export const updateNarrative = async (
 };
 
 export const deleteNarrative = async (
-  narrative: Partial<NarrativeDto>,
+  narrative: DeleteNarrativeDto,
 ): Promise<Narrative> => {
   const deletedNarrative = await prisma.narrative.delete({
     where: { id: narrative.id },
