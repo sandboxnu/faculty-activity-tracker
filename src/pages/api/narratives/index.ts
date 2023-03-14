47c02876b6349a81be5ac196a9a01b7ef4844b51
Christina Long <long.c@husky.neu.edu>
2023-03-14 19:28:24 -0400

    clarified data types

--- src/pages/api/narratives/index.ts
+++ src/pages/api/narratives/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import {
   CreateNarrativeDto,
   UpdateNarrativeDto,
-  NarrativeDto,
+  DeleteNarrativeDto,
 } from '@/models/narrative.model';
 import {
   createNarrative,
@@ -34,10 +34,8 @@ export default async function handler(
     } catch (e) {
       res.status(500).json({ error: e });
     }
-    // this should be diff if error
-    // -> await error
   } else if (req.method === 'PUT') {
-    const updatedNarrativeDto = req.body as Partial<NarrativeDto>;
+    const updatedNarrativeDto = req.body as UpdateNarrativeDto;
     try {
       const updatedNarrative = await updateNarrative(updatedNarrativeDto);
       res.status(200).json({ data: bigintToJSON(updatedNarrative) });
@@ -46,7 +44,7 @@ export default async function handler(
     }
   } else if (req.method === 'DELETE') {
     // all fields (except userId) should be optional which is the same as UpdateNarrativeDto
-    const deletedNarrativeDto = req.body as Partial<NarrativeDto>;
+    const deletedNarrativeDto = req.body as DeleteNarrativeDto;
     try {
       const deletedNarrative = await deleteNarrative(deletedNarrativeDto);
       res.status(200).json({ data: bigintToJSON(deletedNarrative) });
