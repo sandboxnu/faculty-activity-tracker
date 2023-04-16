c89db37d81a016c15300c61c26e86fe3c4ed4502
Max Pinheiro <maxpinheiro181@gmail.com>
2023-04-16 12:34:07 -0400

    add signout to header; remove logo

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
+  const { data: session } = useSession();
+  const signedIn = session?.user.id !== undefined;
+
   return (
-    <div className="flex items-baseline w-screen bg-black font-bold px-8 py-6">
-      <Image
-        src="/media/logo.svg"
-        alt="Logo"
-        width={50}
-        height={50}
-        className="mr-4"
-      />
+    <div className="flex items-baseline w-screen bg-black font-bold px-5 py-3">
       <Image src="/media/neuLogo.svg" alt="CAMD Logo" width={200} height={50} />
+      {signedIn && (
+        <Link
+          href="/api/auth/signout"
+          className="px-3 py-2 bg-red text-white rounded-xl my-auto ml-auto mr-5"
+        >
+          Sign Out
+        </Link>
+      )}
     </div>
   );
 };
