1a8fa020d6c26a05f470cce0e4ffefe4f22189d0
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-09-29 12:59:59 -0400

    Add favorite activities to Narratives page (#52)
    * feat: add favorites carousel to narratives page
    
    * fix: fix arrow pointer functionality in activity carousel
    
    * fix: scroll to the left if page is blank

--- src/pages/narratives/[category].tsx
+++ src/pages/narratives/[category].tsx
@@ -1,13 +1,15 @@
-import { ResponseStatus } from '@/client/activities.client';
+import { updateActivity, ResponseStatus } from '@/client/activities.client';
 import { createNarrative, updateNarrative } from '@/client/narratives.client';
+import ActivityCarousel from '@/components/Submissions/ActivityCarousel';
 import {
   CreateNarrativeDto,
   NarrativeDto,
   UpdateNarrativeDto,
 } from '@/models/narrative.model';
+import { getActivitiesByQuery } from '@/services/activity';
 import { getNarrativeForUserForCategory } from '@/services/narrative';
 import { bigintToJSON, toTitleCase } from '@/shared/utils/misc.util';
-import { NarrativeCategory } from '@prisma/client';
+import { Activity, ActivityCategory, NarrativeCategory } from '@prisma/client';
 import moment from 'moment';
 import { GetServerSideProps } from 'next';
 import { getSession, useSession } from 'next-auth/react';
@@ -20,6 +22,7 @@ import { useSelector } from 'react-redux';
 interface NarrativeFormProps {
   newNarrative?: boolean;
   narrative?: NarrativeDto;
+  activities?: Activity[];
   error?: string;
 }
 
@@ -32,15 +35,35 @@ export const getServerSideProps: GetServerSideProps<
   if (!category) {
     return { props: { error: 'Category Not Found' } };
   }
+
   if (userId) {
     const narrative = await getNarrativeForUserForCategory(
-      userId,
+      userId, 
       category.toString().toUpperCase() as NarrativeCategory,
     );
+
+    const activities = await getActivitiesByQuery(
+      {
+        userId: userId,
+        isFavorite: true,
+        category: category.toString().toUpperCase() as ActivityCategory,
+      },
+      { dateModified: 'desc' },
+    );
+
+    const parsedActivities = bigintToJSON(activities);
+
     if (narrative) {
       const parsedNarrative = bigintToJSON(narrative);
-      return { props: { newNarrative: false, narrative: parsedNarrative } };
-    } else return { props: { newNarrative: true } };
+      return {
+        props: {
+          newNarrative: false,
+          narrative: parsedNarrative,
+          activities: parsedActivities,
+        },
+      };
+    } else
+      return { props: { newNarrative: true, activities: parsedActivities } };
   } else {
     return { props: { error: 'User not found.' } };
   }
@@ -49,6 +72,7 @@ export const getServerSideProps: GetServerSideProps<
 const NarrativeForm: React.FC<NarrativeFormProps> = ({
   newNarrative,
   narrative,
+  activities,
   error,
 }) => {
   const { data: session, status } = useSession();
@@ -57,6 +81,9 @@ const NarrativeForm: React.FC<NarrativeFormProps> = ({
   const { category } = router.query;
   const [narrativeInput, setNarrativeInput] = useState(narrative?.text || '');
   const [pageError, setPageError] = useState<string | null>(error || null);
+  const [favActivities, setFavActivities] = useState<Activity[]>(
+    activities || [],
+  );
 
   const create = async () => {
     if (!category || !userId) return false;
@@ -90,13 +117,14 @@ const NarrativeForm: React.FC<NarrativeFormProps> = ({
     else setPageError('Could not submit narrative.');
   };
 
-  if (pageError)
+  if (pageError || activities === undefined) {
     return (
       <p className="w-full text-center text-red-500 mt-20">
         {' '}
         Error: {pageError || 'Unknown Error'}
       </p>
     );
+  }
   if (!category)
     return (
       <p className="w-full text-center text-red-500 mt-20">
@@ -104,6 +132,24 @@ const NarrativeForm: React.FC<NarrativeFormProps> = ({
         Invalid Category{' '}
       </p>
     );
+
+  const unfavoriteActivity = async (
+    activityId: number,
+    isFavorite: boolean,
+  ) => {
+    updateActivity(activityId, { isFavorite }).then((res) => {
+      if (res === ResponseStatus.Unauthorized) setPageError('Unauthorized');
+      else if (res === ResponseStatus.NotFound) setPageError('Not Found');
+      else if (res === ResponseStatus.UnknownError)
+        setPageError('Unknown Error');
+      else {
+        setFavActivities((currActivities) =>
+          currActivities.filter((a) => a.id !== activityId),
+        );
+      }
+    });
+  };
+
   return (
     <div className="w-full">
       <Head>
@@ -118,7 +164,19 @@ const NarrativeForm: React.FC<NarrativeFormProps> = ({
           {moment(Number(narrative.dateModified)).format('MMM D, YYYY')}
         </p>
       )}
-      <h3 className="mt-7"> Narrative: </h3>
+
+      {favActivities.length !== 0 && (
+        <ActivityCarousel
+          activities={favActivities}
+          label="Favourite"
+          favoriteActivity={unfavoriteActivity}
+          displayNewActivity={false}
+          leftPadding={true}
+        />
+      )}
+
+      <h3 className="mt-3"> Narrative: </h3>
+
       <textarea
         className="rounded-xl border border-gray-400 focus-within:outline-none my-2 w-full h-1/2 px-6 py-4"
         value={narrativeInput}
