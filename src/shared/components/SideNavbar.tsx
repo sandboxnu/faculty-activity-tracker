79c4dc62e1d0946c1a1421f5605b6590faeafd08
Rishikesh <rishikesh.kanabar@gmail.com>
2023-10-22 08:02:05 -0400

    feat: remove collapsible sidebar functionality

--- src/shared/components/SideNavbar.tsx
+++ src/shared/components/SideNavbar.tsx
@@ -8,62 +8,44 @@ import { useSession } from 'next-auth/react';
 const SideNavbar: React.FC = () => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const pathname = router.pathname;
   const { category } = router.query;
   const { data: session } = useSession();
   const isAdmin = session?.user.admin;
   const isMerit = session?.user.merit;
-
-  useEffect(() => {
-    // don't think this is the best logic
-    setSubmissionsOpen(
-      category !== undefined ||
-        (pathname.includes('/submissions') && !pathname.includes('new')),
-    );
-  }, [pathname, category]);
-
-  const [submissionsOpen, setSubmissionsOpen] = useState(
-    router.pathname.includes('/submissions') &&
-      !router.pathname.includes('new'),
-  );
 
   const navClass = 'text-base text-black font-bold pb-2 nav-underline relative';
 
   return (
     <div className="flex flex-col items-start min-w-[207px] bg-white font-bold px-6 py-6 space-y-4">
       <Link
-        href= {isMerit ? '/merit/dashboard' : '/dashboard'}
+        href={isMerit ? '/merit/dashboard' : '/dashboard'}
         className={`${navClass} ${
-            router.pathname == `${isMerit ? '/merit/dashboard' : '/dashboard'}`
+          router.pathname == `${isMerit ? '/merit/dashboard' : '/dashboard'}`
             ? 'nav-underline-red'
             : 'hover:nav-underline-red'
         }`}
       >
         Dashboard
       </Link>
-      {!isMerit && (<Link
-        href="/submissions/new"
-        className={`${navClass} ${
-          router.pathname == '/submissions/new'
-            ? 'nav-underline-red'
-            : 'hover:nav-underline-red'
-        }`}
-        onClick={() => dispatch(resetForm())}
-      >
-        Submit a New Activity
-      </Link>
+      {!isMerit && (
+        <Link
+          href="/submissions/new"
+          className={`${navClass} ${
+            router.pathname == '/submissions/new'
+              ? 'nav-underline-red'
+              : 'hover:nav-underline-red'
+          }`}
+          onClick={() => dispatch(resetForm())}
+        >
+          Submit a New Activity
+        </Link>
       )}
-      {!isMerit && (<p
-        className={`${navClass} cursor-pointer hover:border-white`}
-        onClick={() =>
-          category === undefined ? setSubmissionsOpen((b) => !b) : {}
-        }
-      >
-        Submissions
-      </p>
+      {!isMerit && (
+        <p className={`${navClass} cursor-default hover:border-white`}>
+          Submissions
+        </p>
       )}
-      {submissionsOpen && (
-        <div className="flex flex-col items-start space-y-5 pl-4">
+      <div className="flex flex-col items-start space-y-5 pl-4 border-l">
           <Link
             href="/submissions/teaching"
             className={`${navClass} ${
@@ -95,40 +77,42 @@ const SideNavbar: React.FC = () => {
             Service
           </Link>
         </div>
+      {!isMerit && (
+        <Link
+          href="/profile"
+          className={`${navClass} ${
+            router.pathname == '/profile'
+              ? 'nav-underline-red'
+              : 'hover:nav-underline-red'
+          }`}
+        >
+          My Profile
+        </Link>
       )}
-     {!isMerit && (<Link
-        href="/profile"
-        className={`${navClass} ${
-          router.pathname == '/profile'
-            ? 'nav-underline-red'
-            : 'hover:nav-underline-red'
-        }`}
-      >
-        My Profile
-      </Link>
-     )}
-     {isMerit && (<Link
-        href="/merit/professors"
-        className={`${navClass} ${
-          router.pathname == '/merit/professors'
-            ? 'nav-underline-red'
-            : 'hover:nav-underline-red'
-        }`}
-      >
-        Professors
-      </Link>
-     )}
-     {isMerit && (<Link
-        href="/merit/graphs"
-        className={`${navClass} ${
-          router.pathname == '/merit/graphs'
-            ? 'nav-underline-red'
-            : 'hover:nav-underline-red'
-        }`}
-      >
-        Graphs
-      </Link>
-     )}
+      {isMerit && (
+        <Link
+          href="/merit/professors"
+          className={`${navClass} ${
+            router.pathname == '/merit/professors'
+              ? 'nav-underline-red'
+              : 'hover:nav-underline-red'
+          }`}
+        >
+          Professors
+        </Link>
+      )}
+      {isMerit && (
+        <Link
+          href="/merit/graphs"
+          className={`${navClass} ${
+            router.pathname == '/merit/graphs'
+              ? 'nav-underline-red'
+              : 'hover:nav-underline-red'
+          }`}
+        >
+          Graphs
+        </Link>
+      )}
       {isAdmin && (
         <Link
           href="/admin"
