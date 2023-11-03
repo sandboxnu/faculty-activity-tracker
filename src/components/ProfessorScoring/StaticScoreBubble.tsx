0c7fcb864b956c2ad86d65009a932a81747386b0
Rishi Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-11-02 20:45:21 -0400

    Change GitHub Action workflow to only run on Pull Requests and Push to Main (#94)

--- src/components/ProfessorScoring/StaticScoreBubble.tsx
+++ src/components/ProfessorScoring/StaticScoreBubble.tsx
@@ -7,7 +7,7 @@ const StaticScoreBubble: React.FC<{
   return (
     <div className="flex flex-col items-center space-y-2">
       <p className="text-body-bold">{category}</p>
-      <div className="flex h-[44px] w-[44px] w-full items-center justify-center rounded-lg bg-gray-200 shadow-inner shadow-gray-300">
+      <div className="flex h-[44px] w-full items-center justify-center rounded-lg bg-gray-200 shadow-inner shadow-gray-300">
         <div className="flex text-heading-1">{score}</div>
       </div>
     </div>
