77ba20bd386b262be6bf4123b5bd10c125b1ab1c
Rishi Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2023-11-08 18:03:06 -0500

    [Admin page]: Add fields to add/edit access codes for faculty and MC roles (#99)
    * feat(admin): add simple form for setting new access codes
    
    * feat(api): add GET endpoint to get access codes
    
    * feat(admin): fetch the access codes using a `useEffect`
    
    * feat(api): add POST endpoint to set access code
    
    * feat(admin): add update operation to update role access codes
    
    * fix(api): switch from post to put to update access codes
    
    * feat(admin): add user alert to signal successful save
    
    * fix(admin): switch from alert to confirmation

--- src/client/accessCodes.client.ts
+++ src/client/accessCodes.client.ts
@@ -1,7 +1,78 @@
-import { Role } from '@prisma/client';
+import { Role, RoleAccessCode } from '@prisma/client';
 import { ResponseStatus } from './activities.client';
 
 const apiRoot = '/api/access-codes';
+
+export const getAccessCodes = async (): Promise<
+  | RoleAccessCode[]
+  | ResponseStatus.Unauthorized
+  | ResponseStatus.BadRequest
+  | ResponseStatus.NotFound
+  | ResponseStatus.UnknownError
+> => {
+  try {
+    const response = await fetch(`${apiRoot}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+    if (response.ok || response.status === 200) {
+      const data = await response.json();
+      if (data.hasOwnProperty('data')) {
+        return data.data as RoleAccessCode[];
+      } else {
+        return ResponseStatus.UnknownError;
+      }
+    } else if (response.status === 400) return ResponseStatus.BadRequest;
+    else if (response.status === 401) return ResponseStatus.Unauthorized;
+    else if (response.status === 404) return ResponseStatus.NotFound;
+    else return ResponseStatus.UnknownError;
+  } catch (error) {
+    console.log(error);
+    return ResponseStatus.UnknownError;
+  }
+};
+
+export const setAccessCode = async (
+  role: Role,
+  newCode: string,
+): Promise<
+  | RoleAccessCode
+  | ResponseStatus.Unauthorized
+  | ResponseStatus.BadRequest
+  | ResponseStatus.NotFound
+  | ResponseStatus.UnknownError
+> => {
+  try {
+    const response = await fetch(`${apiRoot}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        role: role,
+        newCode: newCode,
+      }),
+    });
+    if (response.ok || response.status === 200) {
+      const data = await response.json();
+      if (data.hasOwnProperty('data')) {
+        return data.data as RoleAccessCode;
+      } else {
+        return ResponseStatus.UnknownError;
+      }
+    } else if (response.status === 400) return ResponseStatus.BadRequest;
+    else if (response.status === 401) return ResponseStatus.Unauthorized;
+    else if (response.status === 404) return ResponseStatus.NotFound;
+    else return ResponseStatus.UnknownError;
+  } catch (error) {
+    console.log(error);
+    return ResponseStatus.UnknownError;
+  }
+};
 
 export const obtainRoleForAccessCode = async (
   accessCode: string,
--- src/services/accessCode.ts
+++ src/services/accessCode.ts
@@ -1,5 +1,28 @@
 import { Role } from '.prisma/client';
+import { RoleAccessCode } from '@prisma/client';
 import prisma from 'lib/db';
+
+export const getAccessCodes = async (): Promise<RoleAccessCode[] | null> => {
+  const roleAccessCodes = await prisma.roleAccessCode.findMany();
+
+  return roleAccessCodes;
+};
+
+export const setAccessCode = async (
+  role: Role,
+  newCode: string,
+): Promise<RoleAccessCode | null> => {
+  const roleAccessCodes = await prisma.roleAccessCode.update({
+    where: {
+      role: role,
+    },
+    data: {
+      accessCode: newCode,
+    },
+  });
+
+  return roleAccessCodes || null;
+};
 
 export const obtainRole = async (accessCode: string): Promise<Role | null> => {
   const roleAccessCode = await prisma.roleAccessCode.findFirst({
