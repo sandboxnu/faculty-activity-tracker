a37098ad0396d17a4ed71ff502975746e10f1a67
Rishikesh <rishikesh.kanabar@gmail.com>
2023-11-21 10:09:30 -0500

    feat(client): implement professor score computation route

--- src/client/professorScore.client.ts
+++ src/client/professorScore.client.ts
@@ -62,3 +62,32 @@ export const getProfessorScoreForUser = async (
     return ResponseStatus.UnknownError;
   }
 };
+
+export const updateComputedProfessorScoreForUser = async (
+  userId: number,
+): Promise<
+  | CreateProfessorScoreDto
+  | ResponseStatus.Unauthorized
+  | ResponseStatus.BadRequest
+  | ResponseStatus.UnknownError
+> => {
+  try {
+    const response = await fetch(apiRoot + '/' + userId, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+
+    if (response.ok || response.status === 200) {
+      const data = await response.json();
+      return data;
+    } else if (response.status === 400) return ResponseStatus.BadRequest;
+    else if (response.status === 401) return ResponseStatus.Unauthorized;
+    else return ResponseStatus.UnknownError;
+  } catch (error) {
+    console.log(error);
+    return ResponseStatus.UnknownError;
+  }
+};
