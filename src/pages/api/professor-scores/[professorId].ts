968973406182126c628aaf6dae71a68c5d1844b6
Rishikesh <rishikesh.kanabar@gmail.com>
2023-11-21 10:00:27 -0500

    feat(api): add PUT /api/professor-scores/[professorId]

--- src/pages/api/professor-scores/[professorId].ts
+++ src/pages/api/professor-scores/[professorId].ts
@@ -1,29 +1,33 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getProfessorScore } from '@/services/professorScore';
+import {
+  computeProfessorScore,
+  getProfessorScore,
+  upsertProfessorScore,
+} from '@/services/professorScore';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const { professorId } = req.query;
+
+  if (!professorId) {
+    res.status(400).json({ error: 'Professor not found' });
+    return;
+  }
+
+  const parsedProfessorId = parseInt(
+    Array.isArray(professorId) ? professorId[0] : professorId,
+    10,
+  );
+
+  if (isNaN(parsedProfessorId)) {
+    res.status(400).json({ error: 'Invalid professor ID' });
+    return;
+  }
+
   if (req.method === 'GET') {
     try {
-      const { professorId } = req.query;
-
-      if (!professorId) {
-        res.status(400).json({ error: 'Professor not found' });
-        return;
-      }
-
-      const parsedProfessorId = parseInt(
-        Array.isArray(professorId) ? professorId[0] : professorId,
-        10,
-      );
-
-      if (isNaN(parsedProfessorId)) {
-        res.status(400).json({ error: 'Invalid professor ID' });
-        return;
-      }
-
       const professorScore = await getProfessorScore(parsedProfessorId);
 
       if (professorScore) {
@@ -31,6 +35,15 @@ export default async function handler(
       } else {
         res.status(404).json({ error: 'Professor score not found' });
       }
+    } catch (e) {
+      res.status(500).json({ error: (e as Error)?.message || 'Unknown error' });
+    }
+  } else if (req.method === 'PUT') {
+    try {
+      const computedScore = await computeProfessorScore(parsedProfessorId);
+      const updatedScore = await upsertProfessorScore(computedScore);
+
+      res.status(200).json(updatedScore);
     } catch (e) {
       res.status(500).json({ error: (e as Error)?.message || 'Unknown error' });
     }
